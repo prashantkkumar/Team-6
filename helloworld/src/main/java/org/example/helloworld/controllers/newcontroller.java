a853af8d196907fb1b59a7f8de5a4eae31b459a9
PrashantKumar_2110991043 <prashant1043.be21@chitkara.edu.in>
2025-06-05 19:30:20 +0530

    Testing for practise

---
+++ helloworld/src/main/java/org/example/helloworld/controllers/newcontroller.java
@@ -0,0 +1,15 @@
+package org.example.helloworld.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class PrashantController {
+
+    @GetMapping("new")
+    public String greetUser() {
+        return "Hello for testing";
+    }
+}
