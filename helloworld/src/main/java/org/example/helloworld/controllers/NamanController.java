f61b4d1fcfb63644ad6b74fa69ec96fcc9405451
NamanGoyal17 <namangoyal.com@gmail.com>
2025-06-05 15:55:57 +0530

    adding controller

---
+++ helloworld/src/main/java/org/example/helloworld/controllers/NamanController.java
@@ -0,0 +1,15 @@
+package com.example.JournalApp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/Naman")
+public class NamanController {
+
+    @GetMapping
+    public String demo(){
+        return "Hello my name is Naman";
+    }
+}
