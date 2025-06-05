098ffb7b640f3ffad002129b2b48ca8ebbd35b1d
Ayush Nagar <ayushnagar633@gmail.com>
2025-06-05 15:48:35 +0530

    [AYUSH]ADD: Initial Commit for AyushNagar

---
+++ helloworld/src/main/java/org/example/helloworld/controllers/AyushController.java
@@ -0,0 +1,15 @@
+package org.example.helloworld.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class AyushController {
+
+    @GetMapping("/ayush")
+    public String greetUser() {
+        return "Hello my name is Ayush Nagar";
+    }
+}
