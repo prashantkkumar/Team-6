974d39a6a2c2de66f8f94a3078786a2b068c1b38
PrashantKumar_2110991043 <prashant1043.be21@chitkara.edu.in>
2025-06-05 19:12:59 +0530

    [Prashant] Structure and Controller Code(2)

--- helloworld/src/main/java/org/example/helloworld/controllers/MohitController.java
+++ helloworld/src/main/java/org/example/helloworld/controllers/MohitController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 
-public class PrashantController {
+public class MohitController {
 
     @GetMapping("Mohit")
     public String greetUser() {
